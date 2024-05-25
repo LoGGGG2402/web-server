bba2d0d866f56298d83fa3f27ed4fcec3215e208
phanphanhailong <long.pph214964@sis.hust.edu.vn>
2024-05-25 10:05:32 +0700

    Swal

--- app.js
+++ app.js
@@ -22,6 +22,11 @@ let apiRouter = require('./src/api/versionRouter');
 // app
 let app = express();
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // middleware
 app.use(cors({

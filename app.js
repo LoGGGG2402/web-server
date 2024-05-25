6f5a7aef466c667167d58a340b35dbc231c8e645
phanphanhailong <long.pph214964@sis.hust.edu.vn>
2024-05-25 10:21:32 +0700

    Swal

--- app.js
+++ app.js
@@ -29,11 +29,12 @@ let app = express();
 // });
 
 // middleware
-app.use(cors({
-    origin: "*",
-    credentials: true,
-    optionsSuccessStatus: 200
-}));
+// app.use(cors({
+//     origin: "*",
+//     credentials: true,
+//     optionsSuccessStatus: 200
+// }));
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));

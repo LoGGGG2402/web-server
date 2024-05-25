d6187cf86095f05dbfeaacc87d5c679d22f927cc
unknown <pgtrungg@gmail.com>
2024-05-25 11:50:16 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 // let cors = require('cors');
 let requestIp = require('request-ip');
+let rateLimit = require('express-rate-limit');
 
 require('dotenv').config();
 
@@ -33,6 +34,19 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
+app.use(rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 300,
+    headers: true, 
+    handler: (req, res, next) => {
+      const retryAfter = Math.ceil((req.rateLimit.resetTime - Date.now()) / 1000);
+      res.set('Retry-After', retryAfter); 
+      res.status(429).json({
+        success: false,
+        message: `You have sent too many requests. Please try again after ${retryAfter} seconds.`,
+      });
+    },
+  }));
 
 
 app.use(logger('dev'));

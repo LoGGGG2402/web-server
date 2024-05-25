d5dcb6912a98cb00913acbdbf4f1a8a471a019dc
phanphanhailong <long.pph214964@sis.hust.edu.vn>
2024-05-25 14:30:29 +0700

    device verify

--- src/config/configPw.js
+++ src/config/configPw.js
@@ -25,17 +25,14 @@ let configPw = () => {
         process.env.EMAIL_VERIFICATION_TOKEN_SECRET = crypto.randomBytes(64).toString('hex');
     }
 
-    if (!process.env.ENCRYPTION_KEY) {
-        // Generate a new ENCRYPTION_KEY
-        // Load the new ENCRYPTION_KEY into the environment variables
-        process.env.ENCRYPTION_KEY = crypto.randomBytes(64).toString('hex');
+    // DEVICE_VERIFICATION_TOKEN_SECRET
+    if (!process.env.DEVICE_VERIFICATION_TOKEN_SECRET) {
+        // Generate a new DEVICE_VERIFICATION_TOKEN_SECRET
+        // Load the new DEVICE_VERIFICATION_TOKEN_SECRET into the environment variables
+        process.env.DEVICE_VERIFICATION_TOKEN_SECRET = crypto.randomBytes(64).toString('hex');
     }
 
-    if (!process.env.SALT) {
-        // Generate a new SALT
-        // Load the new SALT into the environment variables
-        process.env.SALT = crypto.randomBytes(64).toString('hex');
-    }
+
 }
 
 module.exports = configPw;
